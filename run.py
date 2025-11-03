fc4dcea9cc9e60e139b95a0ee6831932076e5d9f
ZUBENATHI-SAMKILE <samkilezubenathi@gmail.com>
2025-11-03 23:43:27 +0200

    change the run.py

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
-from App import create_app
+from App import create_app, db
+from App import models
 
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=False)
